d5efe6a0c972108826f693adeda7ced3e5843c52
Jaime Deverall <jaime1deverall@gmail.com>
2017-11-15 23:47:33 -0800

    possibly working

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import random
+import numpy as np
 
 class Agent:
   """
@@ -18,67 +19,152 @@ class Agent:
     raiseNotDefined()
 
 class Protagonist(Agent):
-  def __init__(self):
-    pass
+  def getAction(self, currentCards, opponentClaims, currentClaims):
+    if len(opponentClaims) == 0:
+      #Play one card and tell the truth
+      randomIndex= self.uniformDraw(currentCards)
+      cards = [1 if i == randomIndex else 0 for i in range(len(currentCards))]
+      claim = (randomIndex, 1)
+      return claim, cards
+    if np.random.rand() < .1: #Call bluff 10% of the time
+      return "Bluff", None
+    claim, cards = self.tellTruth(currentCards, opponentClaims[-1][0])
+    if cards != None:
+      return claim, cards
+    #Must lie. Draw one random card from currentCards and claim same as opponent's last card
+    randomIndex= self.uniformDraw(currentCards)
+    cards = [1 if i == randomIndex else 0 for i in range(len(currentCards))]
+    claim = (opponentClaims[-1][0], 1)
+    return claim, cards
 
-  def getAction(self, gameState):
-    pass
+  def tellTruth(self, currentCards, lastRank):
+    for i in [-1,0,1]:
+      index = (lastRank + i) % len(currentCards)
+      if currentCards[index] > 0:
+        return (index, 1), [1 if i == index else 0 for i in range(len(currentCards))]
+    return None, None
+
+  #Returns index of a random card drawn from currentCards
+  def uniformDraw(self, currentCards):
+    s = float(sum(currentCards))
+    return np.random.choice(len(currentCards), 1, p=[x/s for x in currentCards])[0]
 
 class Contender(Agent):
-  def __init__(self):
-    pass
+  def getAction(self, currentCards, opponentClaims, currentClaims):
+    if len(opponentClaims) == 0:
+      #Play one card and tell the truth
+      randomIndex= self.uniformDraw(currentCards)
+      cards = [1 if i == randomIndex else 0 for i in range(len(currentCards))]
+      claim = (randomIndex, 1)
+      return claim, cards
+    if np.random.rand() < .1: #Call bluff 10% of the time
+      return "Bluff", None
+    claim, cards = self.tellTruth(currentCards, opponentClaims[-1][0])
+    if cards != None:
+      return claim, cards
+    #Must lie. Draw one random card from currentCards and claim same as opponent's last card
+    randomIndex= self.uniformDraw(currentCards)
+    cards = [1 if i == randomIndex else 0 for i in range(len(currentCards))]
+    claim = (opponentClaims[-1][0], 1)
+    return claim, cards
 
-  def getAction(self, gameState):
-    pass
+  def tellTruth(self, currentCards, lastRank):
+    for i in [-1,0,1]:
+      index = (lastRank + i) % len(currentCards)
+      if currentCards[index] > 0:
+        return (index, 1), [1 if i == index else 0 for i in range(len(currentCards))]
+    return None, None
+    
+  #Returns index of a random card drawn from currentCards
+  def uniformDraw(self, currentCards):
+    s = float(sum(currentCards))
+    return np.random.choice(len(currentCards), 1, p=[x/s for x in currentCards])[0]
 
 class Game:
   def __init__(self):
-    self.protagonist = Protagonist()
-    self.playerCards = [[],[]]
+    self.players= [Protagonist(), Contender()]
+    protagonistCards = [0] * 13
+    contenderCards = [0] * 13
+    self.playerCards = [protagonistCards,contenderCards]
     self.playerClaims = [[],[]]
-    self.contender = Contender()
     initialCards = []
     for _ in range(4):
-      for i in range(1,14):
+      for i in range(0,13):
         initialCards.append(i)
     for _ in range(26):
-      self.playerCards[0].append(initialCards.pop(random.randint(0,len(initialCards)-1)))
-      self.playerCards[1].append(initialCards.pop(random.randint(0,len(initialCards)-1)))
-    self.deck = []
+      self.playerCards[0][initialCards.pop(random.randint(0,len(initialCards)-1))]+=1
+      self.playerCards[1][initialCards.pop(random.randint(0,len(initialCards)-1))]+=1
+    self.deck = [0] * 13
     self.currentPlayer = 0
+    self.cardsLastPlayed = None
 
   def run(self):
     while not self.gameEnded():
-      opponentClaims = self.playerClaims[(self.currentPlayer + 1)%2]
+      opponentClaims = self.playerClaims[self.opponentOf(self.currentPlayer)]
       currentPlayerClaims = self.playerClaims[self.currentPlayer]
       currentPlayerCards = self.playerCards[self.currentPlayer]
-      claim, cards = self.currentPlayer.getAction(currentPlayerCards, opponentClaims, currentPlayerClaims)
+      claim, cardsPlayed = self.players[self.currentPlayer].getAction(currentPlayerCards, opponentClaims, currentPlayerClaims)
       if claim == "Bluff":
-        opponentBluffed = didOpponentBluff(opponentClaims[-1])
+        opponentBluffed = self.didOpponentBluff(opponentClaims[-1], self.cardsLastPlayed)
         if opponentBluffed:
-          print("player {} was caught bluffing".format((self.currentPlayer + 1)%2))
-          self.playerClaims = [[],[]]
-          self.playerCards[(self.currentPlayer+1)%2].extend(self.deck) #bluffing player gets deck
-          self.currentPlayer = (self.currentPlayer+1)%2 #the new player is the opponent
+          print("player {} was caught bluffing".format(self.opponentOf(self.currentPlayer)))
+          self.penalise(self.opponentOf(self.currentPlayer))
+          self.reset(self.opponentOf(self.currentPlayer))
         else:
-          print("player {} incorrectly called bluff".format(self.currentPlayer))
-          self.playerClaims = [[],[]]
-          self.playerCards[self.currentPlayer].extend(self.deck)
-          
+          self.penalise(self.currentPlayer)
+          self.reset(self.currentPlayer)
       else:
-        currentPlayerClaims.append(claim)
-        self.deck.extend(cards)
+        self.putDownCards(claim, cardsPlayed)
+        self.currentPlayer = self.opponentOf(self.currentPlayer)
     print("player {} won".format(self.getWinner()))
 
-  def gameEnded(self):
-    return len(self.playerCards[0]) == 0 or len(self.playerCards[1]) == 0
+  def getWinner(self):
+    if sum(self.playerCards[0]) == 0:
+      return 0
+    return 1
 
-  def didOpponentBluff(opponentClaim):
-    return opponentClaim == self.deck[-len(opponentClaim):]
+  def gameEnded(self):
+    return sum(self.playerCards[0]) == 0 or sum(self.playerCards[1]) == 0
+
+  def didOpponentBluff(self, opponentClaim, cardsLastPlayed):
+    cardIndex = opponentClaim[0]
+    return opponentClaim[1] == cardsLastPlayed[cardIndex]
+
+  def opponentOf(self, player):
+    return (player + 1)%2
+
+  def penalise(self, player):
+    self.transferStacks(self.playerCards[player], self.deck)
+
+  def reset(self, nextPlayer):
+    self.playerClaims = [[],[]]
+    self.cardsLastPlayed = None
+    self.currentPlayer = nextPlayer
+
+  def penaliseCurrentPlayer(self):
+    print("player {} incorrectly called bluff".format(self.currentPlayer))
+    self.playerClaims = [[],[]]
+    self.playerCards[self.currentPlayer].extend(self.deck)
+    self.currentPlayer = self.currentPlayer #currentPlayer incorrectly bluffed so its his turn.
+    self.currentRank = None
+
+  #Update deck
+  #Update player's cards
+  #Update last card played
+  #Update claims
+  def putDownCards(self, claim, cardsPlayed):
+    self.moveStacks(self.deck, self.playerCards[self.currentPlayer], cardsPlayed)
+    self.cardsLastPlayed = cardsPlayed
+    self.playerClaims[self.currentPlayer].append(claim)
+
+  #We're gonna move cards from source to target
+  def moveStacks(self, target, source, cards):
+    for i in range(len(target)):
+      target[i] += cards[i]
+      source[i] -= cards[i]
+
+  def transferStacks(self, target, source):
+    self.moveStacks(target, source, source)
 
 game = Game()
-print(game.protagonistCards)
-print(game.opponentCards)
-
-
-
+game.run()
